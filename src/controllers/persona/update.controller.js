c2510a0dbc1424cf11021678e22d27e697466682
Cristhian Rodríguez <crisrodam1996@gmail.com>
2026-03-31 11:34:03 -0500

    Sección de cumpleañeros

--- src/controllers/persona/update.controller.js
+++ src/controllers/persona/update.controller.js
@@ -3,12 +3,15 @@ import { personaService } from '../../services/index.services.js'
 const actualizarPersona = async (req, res) => {
   try {
     const { id } = req.params
-    const data = req.body
+    const { fechaNacimiento } = req.body
+    const fechaAjustada = fechaNacimiento ? `${fechaNacimiento}T12:00:00` : null
+    const data = fechaNacimiento ? { ...req.body, fechaNacimiento: fechaAjustada } : { ...req.body }
     const { code, message } = await personaService.actualizarPersona(id, data)
     res.status(code).json({
       message,
     })
   } catch (error) {
+    console.log(error.message)
     res.status(500).json({
       message: 'Error interno en el servidor. Intente de nuevo',
     })
