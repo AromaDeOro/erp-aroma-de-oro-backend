7a341974bbb93a1be23a62aa4a91165e44e5dbd9
Cristhian Rodríguez <crisrodam1996@gmail.com>
2026-04-02 14:36:14 -0500

    Impresion de comprobantes de gastos

--- src/services/gasto/get.service.js
+++ src/services/gasto/get.service.js
@@ -2,12 +2,19 @@ import { Caja, Gasto, Usuario } from '../../libs/db.js'
 
 const listarGastos = async () => {
   const gastos = await Gasto.findAll({
+    // --- ORDENAMIENTO ---
+    // 'DESC' hace que el registro con la fecha más reciente aparezca primero
+    order: [['createdAt', 'DESC']],
+
     include: [
       {
         model: Caja,
       },
       {
         model: Usuario,
+        // Si no quieres mandar la contraseña al frontend,
+        // puedes excluir atributos aquí por seguridad:
+        attributes: { exclude: ['clave'] },
       },
     ],
   })
